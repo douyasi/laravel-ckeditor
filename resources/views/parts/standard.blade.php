aba32ec315496ff19514949723ce02d05c164ca2
raoyc <raoyc2009@gmail.com>
2018-01-09 11:18:27 +0800

    source first-place

--- resources/views/parts/standard.blade.php
+++ resources/views/parts/standard.blade.php
@@ -1,12 +1,12 @@
     <script type="text/javascript">
         CKEDITOR.replace("{{ $editorId }}", {
             toolbar : [
+                { name: 'document', items: [ 'Source' ] },
                 { name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
                 { name: 'editing', items: [ 'Scayt' ] },
                 { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
                 { name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'SpecialChar' ] },
                 { name: 'tools', items: [ 'Maximize' ] },
-                { name: 'document', items: [ 'Source' ] },
                 '/',
                 { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Strike', '-', 'RemoveFormat' ] },
                 { name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote' ] },
