b2485ccffe763fd01b0f1dd5c85f379f7c4e542a
raoyc <raoyc@foxmail.com>
2022-05-26 11:22:00 +0800

    dump

--- resources/views/example.blade.php
+++ resources/views/example.blade.php
@@ -11,7 +11,7 @@
         &lt;p&gt;This is some sample text. You are using &lt;a href=&quot;http://ckeditor.com&quot;&gt;CKEditor&lt;/a&gt;, an online &lt;abbr title=&quot;What You See Is What You Get&quot;&gt;WYSIWYG&lt;/abbr&gt;&amp;nbsp;editor.&lt;/p&gt;
     </textarea>
     @section('ckeditorExtraScript')
-    // 可以继续追加额外的 CKEditor 代码，具体请参考官方文档 https://sdk.ckeditor.com/ 或 https://docs.ckeditor.com/ckeditor4/docs/
+    // add some addon config, ref: https://sdk.ckeditor.com/ or https://docs.ckeditor.com/ckeditor4/docs/
     @if($mode == 'full')
     extraPlugins : 'uploadimage,image2',
     filebrowserImageUploadUrl : '/laravel-ckeditor/upload?_token={!! csrf_token() !!}&type=images&by=btn_up',
--- src/Http/Controllers/CKEditorController.php
+++ src/Http/Controllers/CKEditorController.php
@@ -10,13 +10,14 @@ class CKEditorController extends Controller
 {
 
     /**
+     * ExamplePage
      * 示例页面
      *
+     * @param  string $mode
      * @return Response
      */
     public function getExample($mode = 'basic')
     {
-
         if (config('app.debug', false)) {
             if (in_array($mode, ['basic', 'standard', 'full', 'customized'])) {
                 return view('ckeditor::example', ['mode' => $mode]);
@@ -30,6 +31,7 @@ class CKEditorController extends Controller
     }
 
     /**
+     * Image Upload for CKEditor Controller
      * 针对 CKEditor 所写的图片上传控制器
      *
      * @param  Request $request
@@ -39,18 +41,20 @@ class CKEditorController extends Controller
     {
         if (config('ckeditor.usingLocalPackageUploadServer', false)) {
             if ($request->hasFile('upload')) {
-                //
                 $file = $request->file('upload');
                 $funcNum = $request->input('CKEditorFuncNum', 0);
-                $by = $request->input('by', 'btn_up');  // btn_up 通过按钮上传 drop_up 浏览器拖曳上传
+                // btn_up : upload by form btn 通过表单按钮上传
+                // drop_up: upload by drop/drag to browser 浏览器拖曳上传
+                // clipboard_up: upload by clipboard/screen shot 截屏到剪切板上传
+                $by = $request->input('by', 'btn_up');
                 $data = $request->all();
                 $rules = [
                     'upload'    => 'mimes:jpeg,png,gif|max:5120',
                 ];
                 $messages = [
-                    'upload.required' => '必须传入文件',
-                    'upload.mimes'    => '文件类型不允许,请上传常规的图片(jpg、png、gif)文件',
-                    'upload.max'      => '文件过大,文件大小不得超出5MB',
+                    'upload.required' => '请传入文件(a file is required)',
+                    'upload.mimes'    => '请上传常规的图片文件(please upload a normal picture file with jpg, png & gif formats)',
+                    'upload.max'      => '文件过大,请少于5MB(file size too large,be less than 5MB)',
                 ];
                 $validator = Validator::make($data, $rules, $messages);
                 if ($validator->passes()) {
@@ -58,18 +62,24 @@ class CKEditorController extends Controller
                     $hash = md5_file($realPath);
                     $destPath = 'uploads/content/';
                     $savePath = $destPath.''.date('Ymd', time());
-                    is_dir($savePath) || mkdir($savePath);  //如果不存在则创建目录
+                    is_dir($savePath) || mkdir($savePath);
                     $name = $file->getClientOriginalName();
                     $ext = strtolower($file->getClientOriginalExtension());
                     $oFile = $hash.'.'.$ext;
-                    $fullfilename = '/'.$savePath.'/'.$oFile;  //原始完整路径
+                    $fullFileName = '/'.$savePath.'/'.$oFile;
                     if ($file->isValid()) {
                         if (in_array($by, ['btn_up', 'drop_or_clipboard_up'])) {
-                            $uploadSuccess = $file->move($savePath, $oFile);  //移动文件
+                            $uploadSuccess = $file->move($savePath, $oFile);
                         }
                         $oFilePath = $savePath.'/'.$oFile;
-                        $_url = $fullfilename;
-                        if ($by === 'drop_or_clipboard_up' || $by === 'btn_up') {  // 拖曳上传或剪切板上传 || 传统表单上传
+                        $_url = $fullFileName;
+                        return response()->json([
+                            'uploaded' => 1,
+                            'fileName' => $name,
+                            'url' => $_url,
+                        ]);
+                        /*
+                        if ($by === 'drop_or_clipboard_up' || $by === 'btn_up') {
                             return response()->json([
                                 'uploaded' => 1,
                                 'fileName' => $name,
@@ -83,6 +93,7 @@ class CKEditorController extends Controller
                                 ]
                             ]);
                         }
+                        */
                     }
                 }
                 $err = $validator->messages()->first();
@@ -91,7 +102,7 @@ class CKEditorController extends Controller
             return response()->json([
                 'uploaded' => 0,
                 'error' => [
-                    'message' => 'upload failed '. $err .'!',
+                    'message' => 'upload failed'. $err .'!',
                 ]
             ]);
         }
@@ -105,10 +116,13 @@ class CKEditorController extends Controller
 
 
     /**
+     * Image Browser for CKEditor Controller
      * 针对 CKEditor 所写的图片浏览控制器
      *
+     * not supported|暂不支持
+     * 
      * @param  Request $request
-     * @return Response
+     * @return Response|string
      */
     public function getBrowser(Request $request)
     {
